b255bc971d2996cc52a95a23c15f9e87657d2ec0
JR <jr@codebakery.ch>
2014-09-30 15:19:26 +0200

    Update floating-labels.js

--- floating-labels.js
+++ floating-labels.js
@@ -9,7 +9,21 @@ var app = angular.module('app', []).
             },
             link: function (scope, element, attrs) {
                 $(element).parents('.form-group').addClass('floating-label-form-group');
-                $(element).before('<label>'+scope.label+'</label>')
+                $(element).before('<label>' + scope.label + '</label>');
+
+                $('body')
+                    .on("input propertychange", ".floating-label-form-group", function (e) {
+                        $(this).toggleClass("floating-label-form-group-with-value", !!$(e.target).val());
+                    });
+                $(element)
+                    .on("focus", function () {
+                        $(this).addClass("floating-label-form-group-with-focus");
+                    });
+                $(element)
+                    .on("blur", function () {
+                        $(this).removeClass("floating-label-form-group-with-focus");
+                    });
+
             }
         }
-    })
+    });
