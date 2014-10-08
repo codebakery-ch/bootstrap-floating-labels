a855a6caa1f384fc12dc8e68b5eb8bc5ddd43ddc
JR <jr@codebakery.ch>
2014-10-08 13:40:08 +0200

    remove jquery, better selectors

--- floating-labels.js
+++ floating-labels.js
@@ -8,26 +8,32 @@
                 pos: '@pos'
             },
             link: function (scope, element, attrs) {
-                //default
-                if(!attrs.pos){
+               //default
+                if (!attrs.pos) {
                     scope.pos = 'top'
                 }
                 
-                $(element).parents('.form-group').addClass('floating-label-form-group');
-                $(element).before('<label class="'+scope.pos+'">' + scope.label + '</label>');
+                var parentRow = element.parents('.form-group');
+                parentRow.addClass('floating-label-form-group');
+                element.before('<label class="' + scope.pos + '">' + scope.label + '</label>');
 
-                $('body')
-                    .on("input propertychange", ".floating-label-form-group", function (e) {
-                        $(this).toggleClass("floating-label-form-group-with-value", !!$(e.target).val());
-                    });
-                $(element)
-                    .on("focus", function () {
-                        $(this).addClass("floating-label-form-group-with-focus");
-                    });
-                $(element)
-                    .on("blur", function () {
-                        $(this).removeClass("floating-label-form-group-with-focus");
-                    });
+                parentRow.toggleClass("floating-label-form-group-with-value", !!element.val());
+
+                element.bind('paste keyup blur focus', function (event) {
+                    var eventType = event.type;
+                    if (eventType == 'focus') {
+                        parentRow.addClass("floating-label-form-group-with-focus");
+                    }
+
+                    if (eventType == 'blur') {
+                        parentRow.removeClass("floating-label-form-group-with-focus");
+                    }
+
+                    if (eventType == 'keyup' || 'paste') {
+                        parentRow.toggleClass("floating-label-form-group-with-value", !!element.val());
+                    }
+
+                });
 
             }
         }
